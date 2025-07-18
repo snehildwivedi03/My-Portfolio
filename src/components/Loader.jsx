5256c0c2317128e506bdbae383bc165b84031dd8
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-07-18 20:17:54 +0530

    Final changes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -18,7 +18,7 @@ const CanvasLoader = () => {
       <p
         style={{
           fontSize: 14,
-          color: "#F1F1F1",
+          color: "#888",
           fontWeight: 800,
           marginTop: 40,
         }}
