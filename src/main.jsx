8fc34f8d72dec32003ed88bc18ec1ee10ac26dd6
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-07-19 03:12:23 +0530

    Final changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Suspense } from "react";
 import "./index.css";
-import CanvasLoader from "./components/Loader.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Suspense fallback={CanvasLoader}>
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>
 );
