c4269c79ef1724165cf6cd0b3beeb320f56faf56
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-07-19 03:27:04 +0530

    Final changes

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
           <Contact />
           {/* <StarsCanvas /> */}
         </div>
-        <HireMe />
+        {/* <HireMe /> */}
       </div>
     </BrowserRouter>
   );
