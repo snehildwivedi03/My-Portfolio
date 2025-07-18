8d811fbf3a93195bf4d6f8bfe36e32a474814a8f
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-07-18 20:20:40 +0530

    Final changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
       </div>
 
       {/* Canvas component - behind everything */}
-      <Suspense fallback={<CanvasLoader />}>
+      <Suspense fallback={<Loader />}>
         <div className="absolute top-0 left-0 w-full h-full z-0">
           <ComputersCanvas />
         </div>
